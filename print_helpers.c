6f50e9a036fb7056f7f1ce5dd79642b691d5753f
Ausaryu <yonas.houriez@gmail.com>
2025-11-24 13:53:44 +0100

    feat: add _putchar function

---
+++ print_helpers.c
@@ -0,0 +1,28 @@
+/**
+ * print_char - Prints a char from a va_list.
+ * @ap: va_list containing the char to print.
+ */
+void print_char(va_list ap)
+{
+	write(1, ,1)
+}
+
+/**
+ * print_string - Prints a string from a va_list.
+ * @ap: va_list containing the string to print.
+ *
+ * Description: Prints (nil) if the string is NULL.
+ */
+void print_string(va_list ap)
+{
+
+}
+
+/**
+ * print_percent - Prints a percent sign.
+ * @ap: va_list (not used).
+ */
+void print_percent(va_list ap)
+{
+
+}
