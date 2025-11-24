e5bb72930aa0e7f2e5716eb22b7dfa3424310184
Ausaryu <yonas.houriez@gmail.com>
2025-11-24 16:17:17 +0100

    made functions in print helper int instead of void

--- print_helpers.c
+++ print_helpers.c
@@ -1,10 +1,17 @@
+#include "main.h"
+#include <stddef.h>
+
 /**
  * print_char - Prints a char from a va_list.
  * @ap: va_list containing the char to print.
+ *
+ * Return: number of charaters printed
  */
-void print_char(va_list ap)
+int print_char(va_list ap)
 {
-	write(1, ,1)
+	_putchar(va_arg(ap, int));
+
+	return (1);
 }
 
 /**
@@ -12,17 +19,32 @@ void print_char(va_list ap)
  * @ap: va_list containing the string to print.
  *
  * Description: Prints (nil) if the string is NULL.
+ * Return: number of charaters printed
  */
-void print_string(va_list ap)
+int print_string(va_list ap)
 {
+	int i;
+	char *string = va_arg(ap, char*);
 
+	if (string != NULL)
+		for (i = 0; string[i] != '\0'; i++)
+		{
+			_putchar(string[i]);
+		}
+
+	return (i);
 }
 
 /**
  * print_percent - Prints a percent sign.
  * @ap: va_list (not used).
+ *
+ * Return: number of charaters printed
  */
-void print_percent(va_list ap)
+int print_percent(va_list ap)
 {
+	(void)ap;
+	_putchar('%');
 
+	return (1);
 }
