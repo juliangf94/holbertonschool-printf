8bcd21956aa4b362255960622cfe50449d3d5798
Ausaryu <yonas.houriez@gmail.com>
2025-11-26 09:28:24 +0100

    moved 0-main.c in test folder and add test in .gitignore

--- test/0-main.c
+++ test/0-main.c
@@ -11,18 +11,22 @@ int main(void)
 {
 	int len;
 	int len2;
-	unsigned int ui;
-	void *addr;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
+
+	len = _printf("Length:[%d, %i]\n", len, len);
+	len2 = printf("Length:[%d, %i]\n", len2, len2);
+
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
 
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
+
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
+
 	_printf("Percent:[%%]\n");
 	printf("Percent:[%%]\n");
 
