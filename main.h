68ba1e4e4031de082519cdf512c7540a72857316
Ausaryu <yonas.houriez@gmail.com>
2025-11-27 10:38:04 +0100

    feat: add function print_hexa lower and upper + make print_int_helper handle hexa

--- main.h
+++ main.h
@@ -14,8 +14,9 @@ typedef struct specifier
 	int (*function)(va_list args);
 } specifier_t;
 
+/** _putchar.c */
 int _putchar(char c);
-
+/** _printf.c */
 int _printf(const char *format, ...);
 int select_type(char type, va_list args);
 /** print_helper.c */
@@ -23,8 +24,12 @@ int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_int(va_list args);
-int print_int_helper(unsigned int int_to_print);
+int print_int_helper(unsigned int int_to_print, unsigned int base,
+	 char *after_nine);
 /** print_helper_2.c */
 int print_unsigned_int(va_list args);
+int print_octal(va_list args);
+int print_hexa_lower(va_list args);
+int print_hexa_upper(va_list args);
 
 #endif
--- print_helper_2.c
+++ print_helper_2.c
@@ -19,7 +19,80 @@ int print_unsigned_int(va_list args)
 	}
 	else
 	{
-		count += print_int_helper(int_to_print);
+		count += print_int_helper(int_to_print, 10, "");
+	}
+
+	return (count);
+}
+
+
+/**
+ * print_octal - Prints a integer.
+ * @args: va_list (not used).
+ *
+ * Return: number of charaters printed
+ */
+int print_octal(va_list args)
+{
+	unsigned int int_to_print = va_arg(args, int);
+	unsigned int count = 0;
+
+	if (int_to_print == 0)
+	{
+		_putchar('0');
+		count++;
+	}
+	else
+	{
+		count += print_int_helper(int_to_print, 8, "");
+	}
+
+	return (count);
+}
+
+/**
+ * print_hexa_lower - Prints a integer in hexa lowercase.
+ * @args: va_list.
+ *
+ * Return: number of charaters printed
+ */
+int print_hexa_lower(va_list args)
+{
+	unsigned int int_to_print = va_arg(args, int);
+	unsigned int count = 0;
+
+	if (int_to_print == 0)
+	{
+		_putchar('0');
+		count++;
+	}
+	else
+	{
+		count += print_int_helper(int_to_print, 16, "abcdefg");
+	}
+
+	return (count);
+}
+
+/**
+ * print_hexa_upper - Prints a integer in hexa uppercase.
+ * @args: va_list.
+ *
+ * Return: number of charaters printed
+ */
+int print_hexa_upper(va_list args)
+{
+	unsigned int int_to_print = va_arg(args, int);
+	unsigned int count = 0;
+
+	if (int_to_print == 0)
+	{
+		_putchar('0');
+		count++;
+	}
+	else
+	{
+		count += print_int_helper(int_to_print, 16, "ABCDEF");
 	}
 
 	return (count);
