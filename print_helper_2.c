62ce659768b57827baf6296c1119bcaace5bf721
juliangf94 <juliangf94@hotmail.com>
2025-11-27 10:35:23 +0100

    Add the print_address in print_helper_2.c file

--- print_helper_2.c
+++ print_helper_2.c
@@ -24,3 +24,43 @@ int print_unsigned_int(va_list args)
 
 	return (count);
 }
+
+
+/**
+ * print_address - Prints the address of a pointer in a
+ * hexadecimal format
+ * @ap: va_list containing the address
+ *
+ * Return: the address in a hexadecimal format
+ */
+int print_address(va_list ap)
+{
+	void *ptr = va_arg(ap, void *); 
+	unsigned long int addr = (unsigned long int) ptr;
+	int count = 0;
+
+	count += _putchar('0');
+	count += _putchar('x');
+	count += _print_hex_long(addr);
+	return (count);
+}
+
+
+/**
+ * _print_hex_long - Prints
+ * @n: 
+ *
+ * Return:
+ */
+int _print_hex_long(unsigned long int n)
+{
+	unsigned long int temp = n;
+	int count;
+	char hex_chars[] = "0123456789abcdef";
+
+	if (temp / 16)
+		count += _print_hex_long(temp / 16);
+		return (count + 1);
+	 _putchar(hex_chars[temp % 16]);
+	return (0);
+}
